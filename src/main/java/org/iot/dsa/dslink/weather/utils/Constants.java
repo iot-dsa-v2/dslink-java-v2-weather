bfa57d2518f38d7c9473f8ab8ec8e1c122efe69f
kanthadonapati <kantha.donapati@gmail.com>
2018-10-16 19:20:27 +0530

    Optimized Code

--- src/main/java/org/iot/dsa/dslink/weather/utils/Constants.java
+++ src/main/java/org/iot/dsa/dslink/weather/utils/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
     public static final String LOW="low";
     public static final String DATE="date";
     public static final String DAY="day";
-    public static final String YAHOO_WEATHER_SERVICE_URL = "https://query.yahooapis.com/v1/public/yql";
+    public static final String YAHOO_WEATHER_SERVICE_URL = "http://query.yahooapis.com/v1/public/yql";
     public static final String WEATHER_DOC_URL = "https://github.com/iot-dsa-v2/dslink-java-v2-weather-develop";
     public static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
     public static final String ACT_REMOVE = "Remove";
